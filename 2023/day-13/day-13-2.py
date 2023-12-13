c5589f5603459ca63f104ae89a02da43adb12be7
NiXerasse <nixerasse@gmail.com>
2023-12-13 16:56:22 +0300

    2023 day-13

---
+++ 2023/day-13/day-13-2.py
@@ -0,0 +1,16 @@
+def get_max_reflection(p):
+    P = len(p)
+    for i in range(1, P):
+        d = min(i, P - i)
+        if sum(sum(a != b for a, b in zip(p[i + di], p[i - 1 - di])) for di in range(d)) == 1:
+            return i
+
+
+with open('input.txt') as f:
+    data = f.read().split('\n\n')
+    total = 0
+    for part in data:
+        part = part.strip().split('\n')
+        h = get_max_reflection(part)
+        total += 100 * h if h else get_max_reflection([*zip(*part)])
+    print(total)
